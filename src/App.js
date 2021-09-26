5e08ca3f7a66a43906f05be55bb21376f06bf5b5
zeeshan asghar <zeeshan.asghar@navigil.com>
2021-09-26 23:12:47 +0300

    material-ui added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { getEvents } from "./MiddlewareAPI";
+import { Button, Typography } from "@mui/material";
 
 import "./App.css";
 
 function App() {
   const [events, setEvents] = useState({});
- 
-  const getEventsData = async () => {
+
+  const handleClick = async () => {
     const eventDetails = await getEvents();
-    
+
     setEvents(eventDetails);
     console.log(eventDetails);
   };
@@ -19,11 +20,19 @@ function App() {
           <p>Events</p>
         </center>
       </h1>
-      <div className="App">{events?.date}</div>
-      <div className="App">{events?.sensor1}</div>
-      <div className="App">{events?.sensor2}</div>
-      <div className="App">{events?.sensor3}</div>
-
+      <div className="App">Date: {events?.date}</div>
+      <div className="App">Sensor1: {events?.sensor1}</div>
+      <div className="App">Sensor2: {events?.sensor2}</div>
+      <div className="App">Sensor3: {events?.sensor3}</div>
+      <Typography align="center">
+        <Button
+          variant="outlined"
+          style={{ alignItems: "center" }}
+          onClick={handleClick}
+        >
+          Get Events
+        </Button>
+      </Typography>
     </>
   );
 }
